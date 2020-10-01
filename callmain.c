c863443d55775ffc331906f13ddc504074f473ee
Nikhil <72146380+nikhilsingh000kj@users.noreply.github.com>
2020-10-01 13:16:36 +0530

    Coding problems
    CPP

---
+++ callmain.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+printf("ajay kr");
+main();
+return 0;
+}
---
+++ check.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+
+   int *b;
+
+   b=6;
+   printf("%u",*b);
+    return 0;
+
+}
